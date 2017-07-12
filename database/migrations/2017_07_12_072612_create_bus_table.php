425b4197923b5a301bec5065ef96a11b908ffbee
Abdul Latif Syafi'i <a.l.syafii@gmail.com>
2017-07-12 14:38:04 +0700

    Create migrations

---
+++ database/migrations/2017_07_12_072612_create_bus_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bus', function (Blueprint $table) {
+            $table->string('id');
+            $table->integer('track_id')->unsigned();
+            $table->string('driver_name');
+            $table->string('nopol');
+            $table->string('color');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bus');
+    }
+}
